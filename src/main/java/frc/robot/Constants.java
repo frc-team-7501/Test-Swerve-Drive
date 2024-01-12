c4082e8a97b667fa124e32c25373f19603d5b164
GreysonAgnello <agngre1130A@mischools.org>
2024-01-11 20:59:57 -0500

    Create Constants for storing variables

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,42 @@
+package frc.robot;
+
+public final class Constants {
+    public static final class CANMapping {
+
+        // DriveTrain 2023 Bot
+        public static final int SPARKMAX_DRIVE_BL = 52;
+        public static final int SPARKMAX_DRIVE_BR = 53;
+        public static final int SPARKMAX_DRIVE_FL = 54;
+        public static final int SPARKMAX_DRIVE_FR = 51;
+
+        public static final int TALONSRX_TURN_BL = 32;
+        public static final int TALONSRX_TURN_BR = 33;
+        public static final int TALONSRX_TURN_FL = 34;
+        public static final int TALONSRX_TURN_FR = 31;
+
+        public static final int TURN_CANCODER_BL = 42;
+        public static final int TURN_CANCODER_BR = 43;
+        public static final int TURN_CANCODER_FL = 44;
+        public static final int TURN_CANCODER_FR = 41;
+        
+        // MISC CAN Bus
+        public static final int PIGEON_IMU = 20;
+        //public static final int PNEUMATIC_HUB = 5;
+    }
+
+    public static final class ControllerMapping {
+        public static final int JOYSTICK = 0;
+        public static final int XBOX = 1;
+    }
+
+    public static final class PneumaticsMapping {
+        // Pneumatics Control Module
+        public static final int PNEUMATIC_SINGLE_SOLENOID_EXTEND = 1;
+        public static final int PNEUMATIC_SINGLE_SOLENOID_RETRACT = 0;
+    }
+
+    public static final class MiscMapping {
+        public static final boolean BRAKE_ON = true;
+        public static final boolean BRAKE_OFF = false;
+    }
+}
